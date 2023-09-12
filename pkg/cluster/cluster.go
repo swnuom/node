d252fa9fed7b26b41baea97389f1931bd29faa6a
swnuom <31702681+swnuom@users.noreply.github.com>
2023-09-12 15:54:27 +0300

    Update cluster.go

--- pkg/cluster/cluster.go
+++ pkg/cluster/cluster.go
@@ -110,8 +110,10 @@ func ListVPNPeer(configURL string) []v1alpha2.VPNPeer {
 	fmt.Printf("ListVPNPeer called with configURL: %s\n", configURL) // Debug
 	config, err := configFromUrl(configURL)
 	check(err)
+	fmt.Printf("configFromUrl ok")
 	clientset, err := versioned.NewForConfig(config)
 	check(err)
+	fmt.Printf("NewForConfig ok")
 	client := clientset.NetworkingV1alpha().VPNPeers()
 	peers, err := client.List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
