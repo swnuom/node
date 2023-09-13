94a63e600ab4d812d1a46812df096a5ab0c704e5
swnuom <31702681+swnuom@users.noreply.github.com>
2023-09-13 10:32:28 +0300

    Update main.go

--- main.go
+++ main.go
@@ -32,6 +32,10 @@ import (
 	"path/filepath"
 	"strings"
 	"time"
+	"io/ioutil"
+	"net/http"
+        "k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 const defaultVPNNetworkV4 = "10.183.0.0/20"
